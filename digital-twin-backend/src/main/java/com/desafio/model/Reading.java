1276e058e3a364c1cfcfdbcd3916c1fa25efad31
emersonbtsilva <emerson_bsilva1@hotmail.com>
2025-05-26 23:13:20 -0300

    feat: improve API structure

--- digital-twin-backend/src/main/java/com/desafio/model/Reading.java
+++ digital-twin-backend/src/main/java/com/desafio/model/Reading.java
@@ -11,16 +11,49 @@ public class Reading {
     private Long id;
 
     private String sensorId;
-    private Double value;
+
+    // @Column(name = "sensor_value") // evita usar "value", que é reservada
+    private Double sensorValue;
+
     private LocalDateTime timestamp;
 
     public Reading() {}
 
-    public Reading(String sensorId, Double value, LocalDateTime timestamp) {
+    public Reading(String sensorId, Double sensorValue, LocalDateTime timestamp) {
         this.sensorId = sensorId;
-        this.value = value;
+        this.sensorValue = sensorValue;
         this.timestamp = timestamp;
     }
 
-    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSensorId() {
+        return sensorId;
+    }
+
+    public void setSensorId(String sensorId) {
+        this.sensorId = sensorId;
+    }
+
+    public Double getSensorValue() {
+        return sensorValue;
+    }
+
+    public void setSensorValue(Double sensorValue) {
+        this.sensorValue = sensorValue;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 }
